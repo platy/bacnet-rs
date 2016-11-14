4251adfd7325236312db91452c6f91f222dea052
Mike Bush <mpbush@gmail.com>
2016-11-14 18:28:23 +0100

    Add whois marshaller

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -4,6 +4,7 @@ use ast::ValueSequence;
 
 trait ServiceMessage where Self: Sized {
     fn unmarshall(body: &ValueSequence) -> Result<Self, UnmarshallError>;
+    fn marshall(&self) -> ValueSequence;
 }
 
 #[derive(Debug, PartialEq)]
@@ -14,6 +15,7 @@ enum UnmarshallError {
 mod whois {
     use ast::ValueSequence;
     use ast::PrimitiveValue::Unsigned;
+    use ast::SequenceableValue::ContextValue;
     use ast::get_context_value;
     use super::ServiceMessage;
     use super::UnmarshallError;
@@ -25,6 +27,12 @@ mod whois {
     }
 
     impl ServiceMessage for Message {
+        fn marshall(&self) -> ValueSequence {
+            vec!(
+                ContextValue(0, Unsigned(self.device_instance_low)), 
+                ContextValue(1, Unsigned(self.device_instance_high)))
+        }
+
         fn unmarshall(body: &ValueSequence) -> Result<Self, UnmarshallError> {
             match (get_context_value(body, 0), get_context_value(body, 1)) {
                 (Some(&Unsigned(low)), Some(&Unsigned(high))) => Ok(Message {
@@ -49,6 +57,20 @@ mod whois {
                        Message::unmarshall(&vec!(
                               ContextValue(0, Unsigned(1)), 
                               ContextValue(1, Unsigned(50000)))));
+        }
+
+        #[test]
+        fn test_marshall_correct() {
+            assert_eq!(vec!(
+                    ContextValue(0, Unsigned(1)),
+                    ContextValue(1, Unsigned(50000))),
+                    Message { device_instance_low: 1, device_instance_high: 50000 }.marshall());
+        }
+
+        #[test]
+        fn test_marshall_cycle() {
+            let message = Message { device_instance_low: 1, device_instance_high: 50000 };
+            assert_eq!(message, Message::unmarshall(&message.marshall()).unwrap());
         }
     }
 }
